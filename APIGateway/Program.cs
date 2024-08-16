0f0d00d7aaf89bb5598e383681312291376df88e
Ritesh <ritesh@gbcommerce.com>
2024-08-16 12:04:08 +0530

    remove unwanted code

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -6,19 +6,12 @@ using Ocelot.Middleware;
 using Ocelot.Provider.Polly;
 
 var builder = WebApplication.CreateBuilder(args);
-//var routes = "Routes";
-//builder.Configuration.AddOcelotWithSwaggerSupport(options =>
-//{
-//    options.Folder = routes;
-//});
 
 builder.Services.AddOcelot(builder.Configuration).AddPolly();
 builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
 var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-//builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
-//    .AddOcelot(routes, builder.Environment)
-//    .AddEnvironmentVariables();
+
 if (builder.Environment.EnvironmentName.ToString().ToLower().Equals("production"))
 {
     builder.Configuration.AddJsonFile("ocelot.Production.json", optional: false, reloadOnChange: true);
@@ -40,7 +33,6 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    //app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -50,20 +42,9 @@ app.UseAuthorization();
 app.UseSwaggerForOcelotUI(options =>
 {
     options.PathToSwaggerGenerator = "/swagger/docs";
-   // options.ReConfigureUpstreamSwaggerJson = AlterUpstreamSwaggerJson;
 
 }).UseOcelot().Wait();
 
 app.MapControllers();
 
 app.Run();
-
-
-
-
- static string AlterUpstreamSwaggerJson(HttpContext context, string swaggerJson)
-{
-    var swagger = JObject.Parse(swaggerJson);
-    // ... alter upstream json
-    return swagger.ToString(Formatting.Indented);
-}
